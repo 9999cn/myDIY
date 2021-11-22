3d3804504844bdd605a9576c8f3b922abbdd9e7e
9999cn <42468683+9999cn@users.noreply.github.com>
2021-11-22 14:36:06 +0800

    Update geo_location.js

--- Quantumult X/Task/geo_location.js
+++ Quantumult X/Task/geo_location.js
@@ -23,7 +23,7 @@
   const paran = ["IP","ISP","地区","城市"]
   $task.fetch(myRequest).then(response => {
     message = response? json2info(response.body,paras) : ""
-      $done({"title": "                IP.SB", "htmlMessage": message});
+      $done({"title": "                  IP.SB", "htmlMessage": message});
   }, reason => {
     message = "</br></br>🛑 查询超时"
     message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;">` + message + `</p>`

21b94f9ba2c3cbdd34333c45c0a34746d9dd90ba
9999cn <42468683+9999cn@users.noreply.github.com>
2021-11-28 22:22:23 +0800

    Update NotabilityProCrack.js

--- Quantumult X/Rewrite/NotabilityProCrack.js
+++ Quantumult X/Rewrite/NotabilityProCrack.js
@@ -1,8 +1,3 @@
-[rewrite_local]
-
-# Notability解锁订阅
 ^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/NotabilityProCrack.js
-
-[mitm] 
 
 hostname = notability.com
